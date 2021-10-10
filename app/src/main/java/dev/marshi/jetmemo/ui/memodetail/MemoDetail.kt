de4e57880b9f6ebfc0f041a13707a69864076c3c
marshi <mashi3648@gmail.como>
2021-10-10 19:47:21 +0900

    player ui.

--- app/src/main/java/dev/marshi/jetmemo/ui/memodetail/MemoDetail.kt
+++ app/src/main/java/dev/marshi/jetmemo/ui/memodetail/MemoDetail.kt
@@ -2,8 +2,10 @@ package dev.marshi.jetmemo.ui.memodetail
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -14,15 +16,19 @@ import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.marshi.jetmemo.ui.NavControllerWrapper
 import dev.marshi.jetmemo.ui.NavControllerWrapperForPreview
@@ -82,19 +88,46 @@ fun MemoDetail(
             onValueChange = { memoTextState.value = it },
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
+                .weight(1f)
         )
+        Player()
     }
 }
 
 @Composable
 fun RecordButtons(onStart: () -> Unit = {}, onStop: () -> Unit = {}) {
-    Row {
+    Row(modifier = Modifier.height(Dp(40f))) {
         Button(onClick = onStart) {
             Text("録音開始")
         }
         Button(onClick = onStop) {
             Text("録音停止")
+        }
+    }
+}
+
+@Composable
+fun Player() {
+    Surface {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1f), onClick = {}) {
+                Text("一時停止")
+            }
+            IconButton(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .wrapContentWidth()
+                    .weight(1f),
+                onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.PlayArrow, null)
+            }
+            Button(modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1f), onClick = {}) {
+                Text("停止")
+            }
         }
     }
 }
@@ -116,4 +149,10 @@ fun MemoDetailPreview() {
             onSave = {}
         )
     }
+}
+
+@Preview
+@Composable
+fun PlayerPreview() {
+    Player()
 }
